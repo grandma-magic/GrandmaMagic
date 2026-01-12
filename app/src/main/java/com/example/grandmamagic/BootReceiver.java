d873346afdc7ec96d2bb374d66fd57b6278b05af
grandma-magic <grandma@blackbox.world>
2026-01-13 01:12:12 +1300

    Update BootReceiver.java

--- app/src/main/java/com/example/grandmamagic/BootReceiver.java
+++ app/src/main/java/com/example/grandmamagic/BootReceiver.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
             Intent serviceIntent = new Intent(context, MagicService.class);
             context.startService(serviceIntent);
         }
